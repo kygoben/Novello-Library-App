18d01934ea19bc4b8be034db9f78bda953a3c217
maximpopov <icecreamguy0@gmail.com>
2022-03-23 09:13:44 -0500

    Alert for login result (success or failure) created. Alert for passwords not matching in account creation created to replace a text box.

---
+++ Frontend/NovelloApp/app/src/main/java/com/yn_1/NovelloApp/Activities/CreateAccountActivity.java
@@ -0,0 +1,70 @@
+package com.yn_1.NovelloApp.Activities;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.yn_1.NovelloApp.R;
+import com.yn_1.NovelloApp.VolleyCommand;
+import com.yn_1.NovelloApp.VolleyRequesters.JsonObjectRequester;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Activity for creating an account. Reachable only through the login activity.
+ */
+public class CreateAccountActivity extends AppCompatActivity {
+
+    ConstraintLayout constraintLayout;
+    EditText usernameInput;
+    EditText passwordInput;
+    EditText passwordConfirmInput;
+    Button createAccount;
+    TextView warning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        constraintLayout = findViewById(R.id.constraintLayout);
+        usernameInput = findViewById(R.id.username);
+        passwordInput = findViewById(R.id.password);
+        passwordConfirmInput = findViewById(R.id.confirmPassword);
+        createAccount = findViewById(R.id.createAccount);
+
+        createAccount.setOnClickListener(v -> {
+            JsonObjectRequester userAddRequester = new JsonObjectRequester();
+            String username = usernameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+            String passwordConfirmation = passwordConfirmInput.getText().toString();
+            if (password.equals(passwordConfirmation)) {
+                JSONObject userJson = new JSONObject();
+                try {
+                    userJson.put("username", username);
+                    userJson.put("password", password);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                //todo: correct URL in next line
+                userAddRequester.postRequest("addUser", userJson, null, null, null);
+                //todo: upon successful account creation go to login screen and give alert that created successfully
+            }
+            else {
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setMessage("Account not created! Passwords do not match!");
+                alert.show();
+            }
+        });
+
+    }
+
+}
